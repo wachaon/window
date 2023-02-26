f1e02b05c1d1d96c95786a06a25824cf2aa1e550
wachaon@gmail.com <wachaon@gmail.com>
2023-02-26 15:44:35 +0900

    Change the content to match the csharpscript module

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
-# window
-control windows with wsh
+# windwo
+*window* は [*wes*](https://github.com/wachaon/wes) でウインドウを操作するモジュールです。
+
+## インストール
+
+```bash
+wes install @wachaon/window --bare
+```
+
+## コンパイル
+*window* は付属のC#のソースコードをコンパイルせずに実行できますが、コンパイルすることで
+より高速に動作可能になります。
+
+```bash
+wes window --compile
+```
+
+既定値ではコンパイルされた実行ファイルは *window.exe* としてカレントディレクトリに配置されます。
+より高度なコンパイルが必要な場合は `csharpscript/compile(input, options)` を使用してください。
+
+## `move(left, top, width, height)`
+
+アクティブなウインドウの位置とサイズを変更します。
+
+## `get()`
+
+アクティブなウインドの位置とサイズをコンソールに表示します。
+*window.exe* をコンパイルしないと使用できません。
+
+```javascript
+const { ger } = require('window')
+get()
+```
+
+## `hwnd()`
+
+アクティブなウインドのウインドウハンドルを取得します。
+
+```javascript
+const { hwnd } = require('window')
+hwnd()
+```
+
+## `windowLeft()` `windowTop()` `windowWidth()` `windowHeight()`
+
+アクティブなウインドの位置やサイズを個別に取得します。
+
+```javascript
+const { windowLeft, windowTop, windowWidth, windowHeight } = require('window')
+console.log('left: %O top: %O width: %O height: %O', windowLeft, windowTop, windowWidth, windowHeight)
+```
+## `max` `min` `normal`
+
+アクティブなウインドをそれぞれ「最大化」、「最小化」、「ノーマル」状態に移行します。
+
+```javascript
+const { max, min, normal } = require('window')
+max()
+normal()
+min()
+```
+
+## `activateHandle(hWnd)`
+
+ウインドウハンドル(`hWnd`) のウインドウをアクティブにします。
+アクティブにしたウインドウが最小化されていたら、それは解除されます。
+
+```javascript
+const { activateHandle, hwnd } = require('window')
+
+WShell.Run('notepad', 1, false)
+WScript.Sleep(500)
+const notepad = hwnd()
+min()
+activateHandle(notepad)
+```
+
+## `activateTitle`
+
+ウインドウタイトルからウインドウをアクティブにします。
+より確実にウインドウをアクティブにする為に実行ファイルがコンパイルされて居なくてもコンパイルしてから実行します。
+
+```javascript
+const { activateTitle } = require('window')
+
+WShell.Run('notepad', 1, false)
+WScript.Sleep(500)
+min()
+const title = '無題 - メモ帳'
+activateTitle(title)
+```
--- index.js
+++ index.js
@@ -1,69 +1,73 @@
-const { readFileSync, existsFileSync, deleteFileSync } = require('filesystem')
-const { resolve, basename, extname } = require('pathname')
+const { existsFileSync, deleteFileSync } = require('filesystem')
+const { resolve } = require('pathname')
 const { execCommand } = require('utility')
-const ps = require('ps')
+const argv = require('argv')
+const isCLI = require('isCLI')
+const { execScript, compile } = require('csharpscript')
 
 // window
-const window_cs = generate('src/window.cs', 5)
+const window_cs = resolve(__dirname, 'src/window.cs')
 const window_exe = resolve(__dirname, 'window.exe')
 const exists_window_exe = existsFileSync(window_exe)
+const window = 'window'
+const Main = 'Main'
 
 // window method
 function move(left = 0, top = 0, width = 100, height = 100) {
     if (exists_window_exe) execCommand(`${window_exe} move ${left} ${top} ${width} ${height}`)
-    else ps(window_cs, ['move', left, top, width, height])
+    else execScript(window_cs, window, Main, 'move', left, top, width, height)
 }
 
 function get() {
     if (exists_window_exe) return execCommand(`${window_exe} get`)
-    else return ps(window_cs, ['get', 0, 0, 0, 0])
+    else execScript(window_cs, window, Main, 'get', 0, 0, 0, 0)
 }
 
 function hwnd() {
     if (exists_window_exe) return execCommand(`${window_exe} hwnd`)
-    else return ps(window_cs, ['hwnd', 0, 0, 0, 0])
+    else return execScript(window_cs, window, Main, 'hwnd', 0, 0, 0, 0)
 }
 
 function windowLeft() {
     if (exists_window_exe) return execCommand(`${window_exe} windowLeft`)
-    else return ps(window_cs, ['windowLeft', 0, 0, 0, 0])
+    else return execScript(window_cs, window, Main, 'windowLeft', 0, 0, 0, 0)
 }
 
 function windowTop() {
     if (exists_window_exe) return execCommand(`${window_exe} windowTop`)
-    else return ps(window_cs, ['windowTop', 0, 0, 0, 0])
+    else return execScript(window_cs, window, Main, 'windowTop', 0, 0, 0, 0)
 }
 
 function windowWidth() {
     if (exists_window_exe) return execCommand(`${window_exe} windowWidth`)
-    else return ps(window_cs, ['windowWidth', 0, 0, 0, 0])
+    else return execScript(window_cs, window, Main, 'windowWidth', 0, 0, 0, 0)
 }
 
 function windowHeight() {
     if (exists_window_exe) return execCommand(`${window_exe} windowHeight`)
-    else return ps(window_cs, ['windowHeight', 0, 0, 0, 0])
+    else return execScript(window_cs, window, Main, 'windowHeight', 0, 0, 0, 0)
 }
 
 function max() {
     if (exists_window_exe) execCommand(`${window_exe} max`)
-    else ps(window_cs, ['max', 0, 0, 0, 0])
+    else execScript(window_cs, window, Main, 'max', 0, 0, 0, 0)
 }
 
 function min() {
     if (exists_window_exe) execCommand(`${window_exe} min`)
-    else ps(window_cs, ['min', 0, 0, 0, 0])
+    else execScript(window_cs, window, Main, 'min', 0, 0, 0, 0)
 }
 
 function normal() {
     if (exists_window_exe) execCommand(`${window_exe} normal`)
-    else ps(window_cs, ['normal', 0, 0, 0, 0])
+    else execScript(window_cs, window, Main, 'normal', 0, 0, 0, 0)
 }
 
 function activateTitle(title) {
     if (exists_window_exe) execCommand(`${window_exe} activateTitle "${title}"`)
     // else ps(window_cs, ['activateTitle', title, 0, 0, 0])
     else {
-        require('./src/compile')('src/window.cs')
+        compile(window_cs, { out: window_exe })
         execCommand(`${window_exe} activateTitle "${title}"`)
         WScript.Sleep(1)
         deleteFileSync(window_exe)
@@ -72,23 +76,12 @@ function activateTitle(title) {
 
 function activateHandle(hWnd) {
     if (exists_window_exe) execCommand(`${window_exe} activateHandle ${hWnd}`)
-    else ps(window_cs, ['activateHandle', hWnd, 0, 0, 0])
+    else execScript(window_cs, window, Main, 'activateHandle', hWnd, 0, 0, 0)
 }
 
-function generate(spec, len = 0) {
-    const file = resolve(__dirname, spec)
-    const program = basename(file, extname(file))
-    const args = len ? (new Array(len)).fill(0).map((arg, i) => `$args[${i}]`).join(', ') : ''
-    const source = readFileSync(file, 'auto')
-    const code = `$Source = @"
-${source}"@
-
-Add-Type -Language CSharp -TypeDefinition $Source
-[${program}]::Main(${args})`
-    return code
-}
-
-module.exports = {
+if (isCLI(__filename)) {
+    if (argv.get('c') || argv.get('compile') || argv.unnamed[1] === 'compile') compile(window_cs, { out: window_exe })
+} else module.exports = {
     move,
     get,
     hwnd,
